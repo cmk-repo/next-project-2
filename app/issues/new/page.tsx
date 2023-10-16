22a2948e8759eaaa3ce656a01c3fa443f53171ec
cmk <contact@chandan.fyi>
2023-10-16 01:59:53 -0400

    extracr error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { Callout } from '@radix-ui/themes';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchema';
 import { z } from 'zod'
+import ErrorMessage from '@/app/api/issues/compoenents/ErrorMessage';
 
 // interface IssueForm {
 //     title: string;
@@ -52,13 +53,20 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder='Title' {...register('title')} />
                 </TextField.Root>
-                {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+                {//errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+                }
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
                 <Controller name="description"
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder="Description" {...field} />
                     }
                 />
-                {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+                {//errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+                }
+
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+
                 <Button> Submit New Issue </Button>
             </form >
         </div>
